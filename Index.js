6868d06569fe35e841c7d84d2b1393153c5b6e97
MTS766 <103117672+MTS766@users.noreply.github.com>
2023-08-10 20:01:08 +0500

    JavaScript Practice set-2

---
+++ Index.js
@@ -0,0 +1,178 @@
+// QUESTION NO-1 --------->
+
+// let age = prompt("What Is Your Age : ");
+
+// if(age >= 0 && age <=12)
+// {
+//     console.log("You Are A Child : ",age);
+// }
+// else if(age >= 13 && age <=19)
+// {
+//     console.log("You Are A Teenager : ",age);
+// }
+// else if(age >= 20 && age <=30)
+// {
+//     console.log("You Are A Young Adult : ",age);
+// }
+// else
+// {
+//     console.log("You Are A Adult : ",age);
+// }
+
+
+// QUESTION NO-2 --------->
+
+// alert("Hey ! Welcome to The Cofee Shop : ");
+// let cofee = prompt("Please Enter Your Choice 'SMALL' - 'MEDIUM' - 'LARGE'. " );
+
+// if(cofee === "small")
+// {
+//     console.log("You Choosed Small Cofee And Your Bill Is Rs=200 :",cofee);
+// }
+// else if(cofee === "medium")
+// {
+//     console.log("You Choosed Medius Cofee And Your Bill Is Rs=350 :",cofee);
+// }
+// else if(cofee === "large")
+// {
+//     console.log("You Choosed Large Cofee And Your Bill Is Rs=450 :",cofee);
+// }
+// else
+// {
+//     console.log("Invalid choice : ");
+// }
+
+
+// QUESTION NO-3 --------->
+
+// let gpa = prompt("Enter Your GPA : ");
+
+// if(gpa > 4)
+//     {
+//         console.log("Error : ");
+//     }
+
+// else if(gpa > 3.5)
+// {
+//     console.log("Excellent : ",gpa);
+// }
+// else if(gpa >= 3.0 && gpa <= 3.5)
+// {
+//     console.log("Good : ",gpa);
+// }
+// else if(gpa < 3.0)
+// {
+//     console.log("Needs Improvement : ",gpa);
+// }
+
+
+// QUESTION NO-4 --------->
+
+// let math = +prompt("Enter Your math Score : ");
+// let science = +prompt("Enter Your science Score : ");
+// let history = +prompt("Enter Your history Score : ");
+
+// let average =(math+science+history)/3;
+
+// if(average >= 90 && average <= 100)
+// {
+//     console.log("Your Grade Is A : ",average);
+// }
+// else if(average >= 80 && average <= 89)
+// {
+//     console.log("Your Grade Is B : ",average);
+// }
+// else if(average >= 70 && average <= 79)
+// {
+//     console.log("Your Grade Is C : ",average);
+// }
+// else if(average >= 60 && average <= 69)
+// {
+//     console.log("Your Grade Is D : ",average);
+// }
+// else
+// {
+//     console.log("Your Are Failed : ",average);
+// }
+
+
+// QUESTION NO-5 --------->
+
+// let num = prompt("Enter A Number : ");
+
+// if(num > 0)
+// {
+//     console.log("The Number Is Positive : ",num);
+// }
+// else if(num < 0)
+// {
+//     console.log("The Number Is Negative : ",num);
+// }
+// else if(num == 0)
+// {
+//     console.log("The Number Is Zero : ",num);
+// }
+// if(num %2 === 0)
+// {
+//     console.log("And Also The Number Is Even : ",num);
+// }
+// else
+// {
+//     console.log("And Also The Number Is Odd : ",num);
+// }
+
+
+// QUESTION NO-6 --------->
+
+// let useramount = prompt("Enter Your Total Purchase Amount $ : ");
+
+// let Discount = useramount * 0.1;
+// let finalAmount = useramount - Discount;
+
+// if(useramount >= 100)
+// {
+//     console.log("Your Total Amount Is $ : " + "'" + useramount + "'" + " " + "After 10% Discount Your Final Amount Is : ",finalAmount);
+// }
+// else
+// {
+//     console.log("Your Final Price Is $ : " + useramount);
+// }
+
+
+// QUESTION NO-7 --------->
+
+// let weight = prompt("Enter Weight in Kilogram : ");
+// let height = prompt("Enter height in Meters : ");
+
+// let BMI = weight / (height * height)
+
+// if(BMI < 18.5 )
+// {
+//     console.log("You BMI IS : " + "'" + BMI + "'" + " " + "And You Are UnderWeight :");
+// }
+// else if(BMI >= 18.5  && BMI <= 24.9)
+// {
+//     console.log("You BMI IS : " + "'" + BMI + "'" + " " + "And You Are NormalWeight :");
+// }
+// else if(BMI >= 25  && BMI <=29.9)
+// {
+//     console.log("You BMI IS : " + "'" + BMI + "'" + " " + "And You Are OverWeight :");
+// }
+// else
+// {
+//     console.log("You BMI IS : " + "'" + BMI + "'" + " " + "And You Are Obese :");
+// }
+
+
+// QUESTION NO-8 --------->
+
+// let age = prompt("What Is Your Age And Citizenship : ");
+
+// if(age >= 18)
+// {
+//     console.log("Your Are Aligible To Vote : ");
+// }
+// else
+// {
+//     console.log("Your Are Not Aligible To Vote : ");
+// }
